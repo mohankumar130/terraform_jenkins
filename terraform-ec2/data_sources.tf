a2c3d29ea4727424229b8d70c5303572b5fa281f
mohankumar130 <mohankumar130@gmail.com>
2024-11-18 00:01:33 +0530

    updated data

--- terraform-ec2/data_sources.tf
+++ terraform-ec2/data_sources.tf
@@ -7,12 +7,3 @@ data "aws_ami" "aml2_ami" {
     values = ["al2023-ami-2023.6*"]
   }
 }
-data "aws_ami" "nat_ami" {
-  most_recent = true
-  owners = ["amazon"]
-
-  filter {
-    name = "name"
-    values = ["amzn-ami-vpc-nat*"]
-  }
-}
